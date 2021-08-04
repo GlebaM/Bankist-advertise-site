c653849910f6f216f6166012e3a53852af450247
Michał Gleba <GlebaM@users.noreply.github.com>
2021-08-04 09:49:27 +0200

    Building tabbed com

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+//Operations
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,18 +37,72 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// SMOOTH SCROLL
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////////////////////////////
+// Page navigation
+
+// An event occcurs on every element
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+//Using EVENT DELEGATION - event occurs on parent
+//1. Add event listener on common parent element
+//2. Determine what element originated the element
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+/////////////////////////////////////////////////
+//OPERATIONS
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  //Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(el => el.classList.remove('operations__tab--active'));
+  tabsContent.forEach(el => el.classList.remove('operations__content--active'));
+
+  //Active tab
+  clicked.classList.add('operations__tab--active');
+
+  //Active content area
+  const id = clicked.dataset.tab;
+  document
+    .querySelector(`.operations__content--${id}`)
+    .classList.add('operations__content--active');
+});
+
 //////////////////////////
 //CREATING, DELETING and INSERTING elements
 //.insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-// header.prepend(message);
-header.append(message);
+// // header.prepend(message);
+// header.append(message);
 // header.append(message.cloneNode(true));
 
 // header.before(message);
@@ -51,18 +111,18 @@ header.append(message);
 // header.after(message.cloneNode(true));
 
 // Deleting
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', () => message.remove());
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', () => message.remove());
 
-// STYLES
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-// console.log(getComputedStyle(message).color);
+// // STYLES
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// // console.log(getComputedStyle(message).color);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-// console.log(message.style.height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// // console.log(message.style.height);
 
 // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
@@ -95,48 +155,12 @@ message.style.height =
 // //Don't use this because it's overrides all the classes
 // logo.className = 'heyah';
 
-// SMOOTH SCROLL
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  //Current window position
-  // console.log(e.target.getBoundingClientRect());
-  //Current scroll
-  console.log('Current scroll (X/Y)', window.pageYOffset, pageXOffset);
-
-  console.log(
-    'height and witdth viewport',
-    document.documentElement.clientWidth,
-    document.documentElement.clientHeight
-  );
-
-  //Scrolling - current scroll position
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // Scrolling smooth
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-
-  // NEW smooth scroll
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-const h1 = document.querySelector('h1');
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading :D');
-  h1.removeEventListener('mouseenter', alertH1);
-};
-h1.addEventListener('mouseenter', alertH1);
+// const h1 = document.querySelector('h1');
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+// h1.addEventListener('mouseenter', alertH1);
 
 // setTimeout(() => {
 //   h1.removeEventListener('mouseenter', alertH1);
@@ -145,3 +169,59 @@ h1.addEventListener('mouseenter', alertH1);
 // h1.onmouseenter=function(e){
 //   alert('addEventListener: Great! You are reading the heading :D');
 // }
+////CREATING  AN EVENT - CHANGE BACKGROUND RANDOM COLOR ON CLICK
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)}, 0.6)`;
+// // console.log(randomColor(0, 255));
+
+// document.querySelectorAll('.nav__link--color').forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     this.style.backgroundColor = randomColor(0, 255);
+//     console.log('LINK', e.target, e.currentTarget);
+//     console.log(e.currentTarget === this);
+
+//     //Stop propagation - stop bubbling
+//     // In general its not a ver
+//     e.stopPropagation();
+//   });
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor(0, 255);
+//   console.log('CONTAINER', e.target, e.currentTarget);
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor(0, 255);
+//   console.log('NAV', e.target, e.currentTarget);
+// });
+
+////////////////
+// DOM TRAVERSING - looking for relative(closest) elements
+//QS works on every child, grandchild etc
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// childnodes works on direct children
+
+//Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+//closest() metchd works the oposite like queryselector. It finds the closest parent
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// // Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// //For childNodesonsole.log(h1.previousElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// //All the siblings
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(el => {
+//   if (el !== h1) el.style.transform = 'rotate(50deg)';
+// });
