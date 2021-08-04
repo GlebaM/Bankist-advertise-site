f6de06efb7a574ee474ad30d8a8c0f79a7178b4a
Michał Gleba <GlebaM@users.noreply.github.com>
2021-08-04 10:20:10 +0200

    Args passed to navbar event handler

--- script.js
+++ script.js
@@ -92,6 +92,24 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 //////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+//Passing arguments into function
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//////////////////////////
 //CREATING, DELETING and INSERTING elements
 //.insertAdjacentHTML
 
